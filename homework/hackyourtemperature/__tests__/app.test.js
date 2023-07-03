d60b2e14150b94c40f36b23f77d65bbfaa8f098a
Samrawit Girma <samrawitg39333.eg@gmail.com>
2023-07-03 21:08:05 +0200

    changes

--- homework/hackyourtemperature/__tests__/app.test.js
+++ homework/hackyourtemperature/__tests__/app.test.js
@@ -1,15 +1,34 @@
-require = require("esm")(module);
-module.exports = require("./app.test");
-
+require = require('esm')(module);
+module.exports = require('./app.test.js');
 
 const request = require('supertest');
-const app = require('../server');
+const app = require('../server.js');
+
 
 test("GET / should return 'hello from backend to frontend!'", async () => {
   const response = await request(app).get("/");
   expect(response.status).toBe(200);
   expect(response.text).toBe("hello from backend to frontend!");
 });
+
+test("POST /weather should respond with 'City is not found!' if the city is unknown", async () => {
+  const response = await request(app)
+    .post("/weather")
+    .send({ city: "unknownCity" });
+
+  expect(response.status).toBe(200);
+  expect(response.body.weatherText).toBe("City is not found!");
+});
+
+test("POST /weather should respond with a temperature if the city is known", async () => {
+  const response = await request(app)
+    .post("/weather")
+    .send({ city: "Amsterdam" });
+
+  expect(response.status).toBe(200);
+  expect(response.body.weatherText).toMatch(/Current temperature in Amsterdam: \d+°C/);
+});
+
 
   
 
--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,50 +1,7 @@
-import express from "express";
-import fetch from "node-fetch";
-import { keys } from "./sources/keys.js";
+import app from "./app.js";
 
-
-
-
-
-const app = express();
 const port = 3000;
-app.use(express.json());
-
-
-
-app.get("/", (req,res) => {
-    res.send("hello from backend to frontend!")
-});
-
-
-
-app.post("/weather", async (req, res) => {
-    const { city } = req.body;
-  
-    try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${keys.API_KEY}`
-      );
-      const data = await response.json();
-  
-      if (data.cod === "404") {
-        res.send({ weatherText: "City is not found!" });
-      } else {
-        const weatherText = `Current temperature in ${city}: ${data.main.temp}°C`;
-        res.send({ weatherText });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ weatherText: "Error fetching weather data." });
-    }
-  });
-
-
-
-
 
 app.listen(port, () => {
-    console.log(`listening to port ${port}`)
-})
-
-export default app;
+    console.log(`Listening on port ${port}`);
+});
